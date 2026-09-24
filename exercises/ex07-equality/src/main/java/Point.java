af69e20444d27368c593fb32c94cac31ebbad555
ayushmangupta624 <ayushman2346@gmail.com>
2026-09-24 12:54:52 -0400

    ex 7

--- exercises/ex07-equality/src/main/java/Point.java
+++ exercises/ex07-equality/src/main/java/Point.java
@@ -47,7 +47,8 @@ public class Point {
   @Override
   public String toString() {
     // TODO
-    return "";
+    String pt = "(" + this.x + ", " + this.y + ")";
+    return pt;
   }
 
   /**
@@ -60,6 +61,12 @@ public class Point {
   public boolean equals(Object o) {
     // TODO: check that o is a Point (use `instanceof`), cast it, and compare
     //       the x and y fields.
+    if(o instanceof Point){
+      Point p = (Point) o;
+      if((p.getX() == this.x) && (p.getY() == this.y)){
+        return true;
+      }
+    }
     return false;
   }
 
@@ -72,6 +79,6 @@ public class Point {
   @Override
   public int hashCode() {
     // TODO: Objects.hash(x, y) is an easy way to combine the fields.
-    return 0;
+    return Objects.hash(x, y);
   }
 }
